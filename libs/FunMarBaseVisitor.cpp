26eb961e6aa2b8dd5de0236000b1ba298679e7f8
e-chan-belov <echanbelov@icloud.com>
2025-04-27 21:32:20 +0300

    deleting temp files

--- libs/FunMarBaseVisitor.cpp
+++
@@ -1,7 +0,0 @@
-
-// Generated from FunMar.g4 by ANTLR 4.13.2
-
-
-#include "FunMarBaseVisitor.h"
-
-
--- libs/FunMarVisitor.cpp
+++
@@ -1,7 +0,0 @@
-
-// Generated from FunMar.g4 by ANTLR 4.13.2
-
-
-#include "FunMarVisitor.h"
-
-
